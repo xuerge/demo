40ce461b6fbfb4e218af1daaacbba6cbe417f27f
徐士义 <>
2019-11-14 21:54:52 +0800

    first commit

---
+++ src/test/java/com/xuerge/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.xuerge.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assert.isTrue(true);
+	}
+
+}
